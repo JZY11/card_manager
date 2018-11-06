f5b293b70c68b9a5eff687085ff3f8a66c1555c2
[JZY11] <1291813139@qq.com>
2018-11-06 17:07:03 +0800

    update

--- cards_main.py
+++ cards_main.py
@@ -1,5 +1,8 @@
 
 while True:
+    # 显示功能菜单
+
+
     action_str = input("请选择希望执行的操作：")
     print("您选择的操作是【%s】" %action_str)
 
@@ -7,7 +10,15 @@ while True:
     if action_str in ["1","2","3"]:
         #如果在开发程序时，不希望立刻编写分支内部的代码，就是使用pass关键字，表示一个占位符，能够保证程序的代码结构正确
 
-        pass
+        # 新增名片
+        if action_str == "1":
+            pass
+        # 显示全部
+        elif action_str == "2":
+            pass
+        # 查询名片
+        elif action_str == "3":
+            pass
     # 0 表示的是退出系统
     elif action_str == "0":
         print("欢迎再次使用【名片管理系统】")
