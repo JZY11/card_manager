00a833393ef0e529085d0fc70cec62a0d51ebe8d
[JZY11] <1291813139@qq.com>
2018-12-15 22:17:31 +0800

    update

--- cards_tools.py
+++ cards_tools.py
@@ -14,4 +14,21 @@ def show_menu():
     print("0. 退出系统")
     print("*" * 50)
 
+def new_card():
+    """新增名片"""
+    print("-" * 50)
+    print("新增名片")
+
+    # 1. 提示用户输入名片的详细信息
+    name_str = input("请输入姓名：")
+    phone_str = input("请输入电话：")
+    qq_str = input("请输入QQ：")
+    email_str = input("请输入邮箱：")
+
+    # 2. 使用用户输入的信息来建立一个名片字典
+    card_dict = {"name": name_str,
+                 "phone": phone_str,
+                 "qq" : qq_str,
+                 "email" : email_str}
+
 
