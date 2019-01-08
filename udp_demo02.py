e390eca29f051f88acfa6014db63c3b3a31e0ed9
[JZY11] <1291813139@qq.com>
2019-01-08 10:49:11 +0800

    update

--- udp_demo02.py
+++ udp_demo02.py
@@ -4,9 +4,15 @@ def main():
     """创建一个udp套接字"""
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # 可以使用套接字收发数据
+    # 可以使用套接字收发数据 (发送任意数据，不单单局限于字节数据)
+    # 从键盘获取数据
+    send_data = input("请输入要发送的数据：")
+
+
     destaddr = ("192.168.0.4", 8080)  # 对方的ip和端口  要写在一个元组里
-    udp_socket.sendto(b"hahaha",destaddr) # b"hahaha" ：字节数据
+    #udp_socket.sendto(b"hahaha",destaddr)
+
+    udp_socket.sendto(send_data.encode("utf-8"), destaddr)
     # 关闭套接字
     udp_socket.close()
 
