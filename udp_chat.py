778cc24d8c34bed1d24c8e44f9fb2e6cc4507480
[JZY11] <1291813139@qq.com>
2019-01-09 13:39:41 +0800

    update

--- udp_chat.py
+++ udp_chat.py
@@ -6,15 +6,22 @@ def main():
 
     # 绑定信息
     udp_socket.bind("",7788)
+
     # 循环来处理事情
     while True:
+        # destaddr = ("192.168.0.4", 8080)
+        dest_ip = input("请输入对方的ip：")
+        dest_port = input("请输入对方的port：")
+
         # 从键盘获取要发送的内容
-        send_data = input("请输入要发送的数据：")
-        destaddr = ("192.168.0.4", 8080)
+        send_data = input("请输入要发送的消息：")
 
         # 发送
-        udp_socket.sendto(send_data.encode("utf-8"),destaddr)
+        udp_socket.sendto(send_data.encode("utf-8"),(dest_ip,dest_port))
+
         # 接收并显示
+        recive_data = udp_socket.recvfrom(1024)
+        print("%s:%s" % (str(recive_data[1]),recive_data.decode("utf_8")))
 
 if __name__ == "__main__":
     main()
