7611a0dd9255c9ef55a1297ec392e53e0bb36abd
[JZY11] <1291813139@qq.com>
2019-01-10 14:27:50 +0800

    update

--- tcp_server_demo02.py
+++ tcp_server_demo02.py
@@ -13,17 +13,18 @@ def main():
     # 3.将手机设置为正常的响铃模式(让默认的套接字由主动变为被动 listen)
     tcp_server_socket.listen(128)
 
-    # 4.等待别人的电话到来(等待客户端的连接 accept) accept返回的是一个元组
-    new_client_socket, clientAddr = tcp_server_socket.accept()  # 左边第一个是accept产生的套接字用来为客户端服务，第二个是某客户端连接过来的信息，也是一个元组(客户端ip，客户端port)
+    while True:
+        # 4.等待别人的电话到来(等待客户端的连接 accept) accept返回的是一个元组
+        new_client_socket, clientAddr = tcp_server_socket.accept()  # 左边第一个是accept产生的套接字用来为客户端服务，第二个是某客户端连接过来的信息，也是一个元组(客户端ip，客户端port)
 
-    # 5.接受客户端发送过来的请求
-    receive_data = new_client_socket.recv(1024)  # 接受1024个字节  返回不是元组，是一个普通数据
+        # 5.接受客户端发送过来的请求
+        receive_data = new_client_socket.recv(1024)  # 接受1024个字节  返回不是元组，是一个普通数据
 
-    # 6.恢复一部分数据给客户端
-    tcp_server_socket.send("hahaha")
+        # 6.恢复一部分数据给客户端
+        tcp_server_socket.send("hahaha")
 
-    # 7.关闭套接字
-    new_client_socket.close()
+        # 7.关闭套接字
+        new_client_socket.close()
     tcp_server_socket.close()
 
 if __name__ == "__mian__":
