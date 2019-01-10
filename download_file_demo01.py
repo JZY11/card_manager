c03e0d3d0cf771609bee6d2e0e8b5686af2143f3
[JZY11] <1291813139@qq.com>
2019-01-10 17:39:31 +0800

    update

--- download_file_demo01.py
+++ download_file_demo01.py
@@ -2,6 +2,8 @@ import socket
 
 
 def main():
+    """文件下载"""
+
     # 创建socket套接字
     tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -17,5 +19,18 @@ def main():
 
     # 发送文件下载请求
     tcp_client_socket.send(file_name.encode("utf-8"))
+    
+    # 接收对方发送回来的数据，最大1024个字节(1k)
+    recv_data = tcp_client_socket.recv(1024)
+    # print('接收到的数据为：', recv_data.decode('utf-8'))
+
+    # 如果真的接收到了数据再创建文件，否则不创建
+    if recv_data:
+        with open("[接收]" + file_name,"wb") as f:
+            f.write(recv_data)
+        # with 这种形式不用写 close()
+    # 关闭套接字
+    tcp_client_socket.close()
+
 if __name__ == "main":
     main()
