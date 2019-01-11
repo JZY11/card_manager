7a018ea942e4fb1e8f316791ec0ea6f8f56fab21
[JZY11] <1291813139@qq.com>
2019-01-11 15:14:00 +0800

    update

--- download_file/download_file_client.py
+++ download_file/download_file_client.py
@@ -15,10 +15,10 @@ def main():
     tcp_client_socket.connect((server_ip, server_port))
 
     # 输入需要下载的文件名
-    file_name = input("请输入您要下载的文件名：")
+    download_file_name = input("请输入您要下载的文件名：")
 
     # 发送文件下载请求
-    tcp_client_socket.send(file_name.encode("utf-8"))
+    tcp_client_socket.send(download_file_name.encode("utf-8"))
     
     # 接收对方发送回来的数据，最大1024个字节(1k)
     recv_data = tcp_client_socket.recv(1024)
@@ -26,7 +26,7 @@ def main():
 
     # 如果真的接收到了数据再创建文件，否则不创建
     if recv_data:
-        with open("[接收]" + file_name,"wb") as f:
+        with open("[接收]" + download_file_name,"wb") as f:
             f.write(recv_data)
         # with 这种形式不用写 close()
     # 关闭套接字
