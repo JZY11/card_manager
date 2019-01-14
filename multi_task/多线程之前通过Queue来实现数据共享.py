38b48135c58f189725bc084d10e8a0abe250bf9c
[JZY11] <1291813139@qq.com>
2019-01-14 23:59:51 +0800

    update

--- multi_task/多线程之前通过Queue来实现数据共享.py
+++ multi_task/多线程之前通过Queue来实现数据共享.py
@@ -1,18 +1,33 @@
 import multiprocessing
 
-def  download_from_web():
+def  download_from_web(q):
     """下载数据"""
     # 模拟从网上下载的数据
     data = [11, 22, 33, 44]
 
+    # 向队列中写入数据
+    for temp in data:
+        q.put(temp)
 
-def analysis_data():
+    print("--- 已经下载完了数据并且存入到了队列中 --")
+
+
+def analysis_data(q):
     """数据处理"""
+    # 从队列中获取数据
+    q.get()
+
+    # 模拟数据处理
 
 
 def  main():
-    p1 = multiprocessing.Process(target=download_from_web)
-    p2 = multiprocessing.Process(target=analysis_data)
+    # 1. 创建一个队列
+    q = multiprocessing.Queue()
+
+    # 2. 创建多个进程，将队列的引用当做实参传递到里面
+
+    p1 = multiprocessing.Process(target=download_from_web, args=(q,))
+    p2 = multiprocessing.Process(target=analysis_data, args=(q,))
 
     p1.start()
     p2.start()
