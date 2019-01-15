d44a384cf4168482e7704bc06af1f02d4989addc
[JZY11] <1291813139@qq.com>
2019-01-15 14:03:27 +0800

    update

--- multi_task/多任务文件夹的copy.py
+++ multi_task/多任务文件夹的copy.py
@@ -1,23 +1,26 @@
 import os
 import multiprocessing
 
-def copy_file(file_name):
+def copy_file(file_name, old_folder_name, new_folder_name):
     """完成文件的复制"""
-    print("--- 模拟copy文件 ---")
-
+    print("--- 模拟copy文件 %s---" % file_name)
+    old_f = open(old_folder_name + "/" + file_name, "rb")     # 打开这个文件夹下的某一个文件
+    print(old_f.read())
+    old_f.close()
 
 def main():
     # 1. 获取要copy的文件夹的名字
-    old_folder = input("请输入要copy的文件夹的名字:")
+    old_folder_name = input("请输入要copy的文件夹的名字:")
 
     # 2. 创建一个新的文件夹
     try:
-        os.mkdir(old_folder + "[复件]")
+        new_folder_name = old_folder_name + "[复件]"
+        os.mkdir(new_folder_name)
     except:
         pass
 
     # 3. 获取文件夹的所有带copy的文件名字 listdir()
-    file_names = os.listdir(old_folder)
+    file_names = os.listdir(old_folder_name)
     print(file_names)
 
     # 4. 创建进程池
@@ -25,9 +28,10 @@ def main():
 
     # 5. 向进程池中添加 copy文件的任务
     for file_name in file_names:
-        po.apply_async(copy_file, args=(file_name, ))
+        po.apply_async(copy_file, args=(file_name, old_folder_name, new_folder_name))
 
-    # 复制原文件夹下的所有文件到新的文件夹中
+    po.close()
+    po.join()
 
 
 if __name__ == "__main__":
