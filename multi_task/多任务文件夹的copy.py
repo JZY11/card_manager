ee8affe533590430ef4114225368f2b39b235726
[JZY11] <1291813139@qq.com>
2019-01-16 10:48:57 +0800

    update

--- multi_task/多任务文件夹的copy.py
+++ multi_task/多任务文件夹的copy.py
@@ -1,7 +1,7 @@
 import os
 import multiprocessing
 
-def copy_file(file_name, old_folder_name, new_folder_name):
+def copy_file(q, file_name, old_folder_name, new_folder_name):
     """完成文件的复制"""
     print("--- 模拟copy文件 %s---" % file_name)
     old_f = open(old_folder_name + "/" + file_name, "rb")     # 打开这个文件夹下的某一个文件
@@ -12,6 +12,8 @@ def copy_file(file_name, old_folder_name, new_folder_name):
     new_f.write(content)
     new_f.close()
 
+    # 如果拷贝完了文件就向队列中写入一个消息，表示已经完成
+    q.put(file_name)
 
 def main():
     # 1. 获取要copy的文件夹的名字
@@ -31,13 +33,19 @@ def main():
     # 4. 创建进程池
     po = multiprocessing.Pool(5)
 
-    # 5. 向进程池中添加 copy文件的任务
+    # 5. 创建一个队列
+    q = multiprocessing.Manager().Queue()
+
+    # 6. 向进程池中添加 copy文件的任务
     for file_name in file_names:
-        po.apply_async(copy_file, args=(file_name, old_folder_name, new_folder_name))
+        po.apply_async(copy_file, args=(q, file_name, old_folder_name, new_folder_name))
 
     po.close()
-    po.join()
+    # po.join()
 
+    while True:
+        file_name = q.get()
+        print("已经完成copy：%s" % file_name)
 
 if __name__ == "__main__":
     main()
