1d2e11ae04bc739d7fa251e62584063fe007c82e
[JZY11] <1291813139@qq.com>
2019-01-16 16:47:16 +0800

    update

---
+++ generation/__init__.py
@@ -0,0 +1,31 @@
+# 生成器是一类特殊的迭代器
+
+# 列表推导式
+nums = [x*2 for x in range(10)]
+print(str(nums))
+
+# 生成器方式1. 将列表推导式的中括号换成小括号
+nums = (x*2 for x in range(10)) # 这就是一个生成器(返回的只是生成数据的方式，节省空间)
+print(nums)
+
+for num in nums:
+    print(num)
+
+
+# 生成器方式2.
+def creat_num(all_num):
+    a, b = 0, 1
+    current_num = 0
+
+    while current_num < all_num:
+        yield a         # 如果一个函数中有yield语句，那么这个就不再是函数，而是一个生成器的模板
+        a, b = b, a + b
+        current_num += 1
+
+# 如果在调用creat_num的时候，发现这个函数有 yield ，那么此时这个就不再是函数，而是创建一个生成器的对象
+obj = creat_num(10)
+
+for num in obj:
+    print(num)
+
+
