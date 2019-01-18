932079509db6d2be2c4840f1e4674ad1047b3847
[JZY11] <1291813139@qq.com>
2019-01-18 10:25:29 +0800

    update

--- generation/生成器(send启动).py
+++ generation/生成器(send启动).py
@@ -8,10 +8,12 @@ def create_num(all_num):
         current_num += 1
 
 
-obj2 = create_num(10)
+obj2 = create_num(10) # 生成器对象
 
 ret = next(obj2)
+# 程序执行 next() ,然后从create_num() 方法开始执行，当执行到 ret = yield a 语句时  暂停不在往下执行  而是执行 print(ret)
 print(ret)
 
 ret = obj2.send("hahah")
+# 程序执行到  生成器对象调用 send() 方法时 又重新回到上次暂停处， 把 send(XX) 中的参数XX 赋值给 yield a  即：ret 然后接着往下执行print(">>>>>>>>>>", ret) 。。。
 print(ret)
