62e67cef15cb3fa3d8385b0a1df1278085e8479b
[JZY11] <1291813139@qq.com>
2019-01-19 17:35:02 +0800

    update

--- generation/gevent版本的图片下载器.py
+++ generation/gevent版本的图片下载器.py
@@ -1,11 +1,23 @@
 import urllib.request
+import gevent
+from gevent import monkey
+
+
+monkey.patch_all()
+
+
+def downloader(img_name, img_rul):
+    req = urllib.request.urlopen(img_rul)
+    image_content = req.read()
+    with open(img_name, "wb") as f:
+        f.write(image_content)
 
 
 def main():
-    req = urllib.request.urlopen("https://rpic.douyucdn.cn/live-cover/roomCover/2018/12/21/a77342abaa632c7760684a6a99f1f387_big.png")
-    image_content = req.read()
-    with open("1.jpg", "wb") as f:
-        f.write(image_content)
+    gevent.joinall([
+        gevent.spawn(downloader, "1.jpg", 'https://rpic.douyucdn.cn/live-cover/roomCover/2018/12/21/a77342abaa632c7760684a6a99f1f387_big.png'),
+        gevent.spawn(downloader,  "2.jpg", 'https://rpic.douyucdn.cn/live-cover/appCovers/2019/01/03/3659788_20190103132115_small.jpg'),
+    ])
 
 
 if __name__ == "__main__":
