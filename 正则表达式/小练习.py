f62475ca0170111f9111288d77e00bd53a3366c1
[JZY11] <1291813139@qq.com>
2019-01-19 19:18:04 +0800

    update

--- 正则表达式/小练习.py
+++ 正则表达式/小练习.py
@@ -1,10 +1,17 @@
 import re
 
-names = ["name1", "_name", "2_name", "_name_"]
 
-for name in names:
-    ret = re.match("[a-zA-Z]+[\w]]*", name)
-    if ret:
-        print("变量名 %s 符合要求" % ret.group())
-    else:
-        print("变量名 %s 非法" % ret.group())
+def main():
+    """正则的match(): 只判断开头不判断结尾"""
+    names = ["age", "_age", "2age", "age1", "a_age", "age_2_", "age!", "____", "a#123"]
+
+    for name in names:
+        ret = re.match(r"^[a-zA-Z_][a-zA-Z0-9_]*$", name)
+        if ret:
+            print("变量名:%s 符合要求...通过正则匹配出来的数据是：%s" % (name, ret.group()))
+        else:
+            print("变量名:%s 非法" % name)
+
+
+if __name__ == "__main__":
+    main()
