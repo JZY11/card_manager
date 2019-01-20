d50846b0f950b24370adcc90f4ea1b9daa86740b
[JZY11] <1291813139@qq.com>
2019-01-20 22:02:45 +0800

    update

--- http协议/返回固定页面的http服务器.py
+++ http协议/返回固定页面的http服务器.py
@@ -1,13 +1,26 @@
 import socket
 
 
+def service_client():
+    """为这个客户端返回数据"""
+
+
 def main():
     """用来完成整体的控制"""
     # 1. 创建套接字
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     # 2. 绑定
+    tcp_server_socket.bind(("", 7890))
+
     # 3. 变为监听套接字
+    tcp_server_socket.listen(128)
+
     # 4. 等待新客户端的链接
+    new_socket, client_address = tcp_server_socket.accept()
+
     # 5. 为这个客户端服务
+    service_client()
 
 
 if __name__ == "__main__":
