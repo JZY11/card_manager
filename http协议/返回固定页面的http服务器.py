cad2636b2ba987e0ca6d2cb4059eca6ed55d18d1
[JZY11] <1291813139@qq.com>
2019-01-20 22:19:36 +0800

    update

--- http协议/返回固定页面的http服务器.py
+++ http协议/返回固定页面的http服务器.py
@@ -1,8 +1,28 @@
 import socket
 
 
-def service_client():
+def service_client(new_socket):
     """为这个客户端返回数据"""
+
+    # 1. 接受浏览器发送过来的请求即http发送过来的请求
+    # GET / HTTP/1.1
+    #......
+
+    request = new_socket.recv(1024)
+    print(request)
+
+    # 2. 返回http格式的数据给浏览器
+    # 2.1 准备好发送给浏览器的数据 ---  header
+    response = "HTTP/1.1 200 0K\r\n"
+    response += "\r\n"
+
+    # 2.2 准备好发送给浏览器的数据 ---  body
+    response += "<h1>hahaha</h1>"
+
+    new_socket.send(response.encode("utf-8"))
+
+    # 关闭套接字
+    new_socket.close()
 
 
 def main():
@@ -16,11 +36,15 @@ def main():
     # 3. 变为监听套接字
     tcp_server_socket.listen(128)
 
-    # 4. 等待新客户端的链接
-    new_socket, client_address = tcp_server_socket.accept()
+    while True:
+        # 4. 等待新客户端的链接
+        new_socket, client_address = tcp_server_socket.accept()
 
-    # 5. 为这个客户端服务
-    service_client()
+        # 5. 为这个客户端服务
+        service_client(new_socket)
+
+    # 关闭监听套接字
+    tcp_server_socket.close()
 
 
 if __name__ == "__main__":
