445b365b6bb7e79b848b282a609c10099e6e8f8c
[JZY11] <1291813139@qq.com>
2019-01-22 10:27:04 +0800

    update

--- 并发web服务器实现/(重要)单进程、线程、非堵塞实现并发的原理.py
+++ 并发web服务器实现/(重要)单进程、线程、非堵塞实现并发的原理.py
@@ -3,14 +3,14 @@ import time
 
 
 tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_server.bind(("", 1111))
+tcp_server.bind(("", 1000))
 tcp_server.listen(128)
 tcp_server.setblocking(False)   # 设置套接字为非堵塞的方式
 
 client_socket_list = list() # 定义一个列表
 
 while True:
-    time.sleep(5)
+    time.sleep(2)
 
     try:
         new_socket, new_addr = tcp_server.accept()
@@ -24,7 +24,7 @@ while True:
 
     for client_socket in client_socket_list:
         try:
-            recv_data = client_socket.recv()
+            recv_data = client_socket.recv(1024)
         except Exception as ret:
             print("---- 这个客户端没有发送来数据 ----")
         else:
