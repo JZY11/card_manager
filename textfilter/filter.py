9425f1243c678ced90d6bf011babe5763aa0fe31
[JZY11] <1291813139@qq.com>
2019-06-25 14:14:17 +0800

    update

--- textfilter/filter.py
+++ textfilter/filter.py
@@ -137,6 +137,10 @@ class DFAFilter:
         import time
         t = time.time()
         print (gfw.filter("法轮功 我操操操", "*"))
+        print (gfw.filter("针孔摄像机 我操操操", "*"))
+        print (gfw.filter("售假人民币 我操操操", "*"))
+        print (gfw.filter("传世私服 我操操操", "*"))
+        print (time.time() - t)
 
         test_first_character()
 
